9da37ba7e53566f9b0295acc481c15694c04c46e
Haseeb <88767579+heyseebo89@users.noreply.github.com>
2021-08-25 22:36:01 +0530

    Add files via upload

---
+++ python datatype1.py
@@ -0,0 +1,19 @@
+a=200 
+Str1 ="hello haseeb"  
+c = 188.346 
+print(type(a))  
+print(type(Str1))  
+print(type(c))
+str1 = 'haseb'      
+str2 = 'haseeb hello'
+print(str1[0:2])
+#list 
+list2 =[ 1,"haseeb",3]
+print(type(list2))
+#tuple
+tup  = ("hi", "Python", 2)    
+# Checking type of tup  
+print (type(tup))     
+print (tup)  
+print (tup[1:])    
+print (tup[0:1])    
